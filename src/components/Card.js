38cf32c70c8ed7f5199b1bb2ff497fc4bb15617b
elijahdaniel <e.paminsan@gmail.com>
2020-09-09 16:05:38 -0700

    styled with sass

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,22 @@ const Card = props => {
   const { title, date, copyright, explanation, url } = props.data
 
   return (
-    <div>
-      <h2 className='title'>{title}</h2>
-      <p className='copyright'>By: {copyright}</p>
-      <p className='date'>Date: {date}</p>
-      <p className='paragraph'>{explanation}</p>
-      <img src={url} alt='APOD' />
+    <div className='card'>
+      <header>
+        <img
+          src='https://lh3.googleusercontent.com/proxy/4kpEh7h5j14KUcCfkwuwGkqBykheU_TCtmx5yePpouEEVFmvWU9r1KEApKlnay4rSVNYkEbJxRF4_8cs443Zqin3jrYfaHYLLmQcdqI5kLbCMnNWm68'
+          alt='logo'
+          className='logo'
+        />
+        <h1>Nasa's Photo of the Day</h1>
+      </header>
+      <div className='content'>
+        <img src={url} className='head-img' alt='APOD' />
+        <h2 className='title'>{title}</h2>
+        <p className='copyright'>By: {copyright}</p>
+        <p className='date'>Date: {date}</p>
+        <p className='paragraph'>{explanation}</p>
+      </div>
     </div>
   )
 }
